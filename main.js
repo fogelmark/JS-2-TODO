89e70d2d60f62e7a13cf98bec8bde94cece08964
fogelmark <108455196+fogelmark@users.noreply.github.com>
2023-01-20 00:11:08 +0100

    fixade checkboxes

--- main.js
+++ main.js
@@ -31,21 +31,28 @@ const printList = () => {
   todosArray.forEach(todos => {
     const listElement = createListElement(todos)
     output.appendChild(listElement)
-    console.log(todos);
   })
 }
 
-/* Skapa listan */
+/* Skapa listans element */
 const createListElement = (todoData) => {
 
   const item = document.createElement('div')
   item.classList.add('item-container')
 
+  const checkbox = document.createElement('input')
+  checkbox.type = 'checkbox'
+  checkbox.classList.add('task', 'task-checkbox')
+
   const task = document.createElement('p')
+  task.classList.add('task', 'task-title')
   task.innerText = todoData.title
 
   const button = document.createElement('button')
+  button.classList.add('delete-button')
   button.innerText = 'delete'
+
+  
 
   button.addEventListener('click', (id) => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
@@ -54,12 +61,12 @@ const createListElement = (todoData) => {
     button.parentElement.remove()
   })
 
+  item.appendChild(checkbox)
   item.appendChild(task)
   item.appendChild(button)
 
   return item
 }
-
 
 const submitTask = e => {
   e.preventDefault()
@@ -77,9 +84,9 @@ const submitTask = e => {
     }
   
     limit++
-    
+
     console.log(newTask);
-  
+      
     fetch(TODO_URL, {
     method: 'POST',
     body: JSON.stringify(newTask),
@@ -98,8 +105,11 @@ const submitTask = e => {
   form.reset()
 }
 
-console.log(todosArray);
+// const updateStatus = e => {
+  
+// }
 
+// output.addEventListener('click', updateStatus)
 form.addEventListener('submit', submitTask)
 
 
